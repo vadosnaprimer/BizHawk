0049c98dc71ba2c92e39b9c897dad089668dcde6
hegyak <hegyak@yahoo.com>
2016-03-06 18:13:54 -0800

    Update VersionInfo.cs to 1.11.6 and Merge Commit 9e3d57341f6a26a04358e6e0ec627425029a1671

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.11.5"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "February 13, 2016";
+	public const string MAINVERSION = "1.11.6"; // Use numbers only or the new version notification won't work
+	public static readonly string RELEASEDATE = "March 7, 2016";
 	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
