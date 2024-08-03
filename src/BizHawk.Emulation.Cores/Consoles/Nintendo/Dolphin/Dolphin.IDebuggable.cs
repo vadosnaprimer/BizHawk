cac78c727d19ee7f3a57096b824ae36c9b494f98
feos <feykomylce@gmail.com>
2024-08-04 00:06:19 +0300

    cycle count

---
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Dolphin/Dolphin.IDebuggable.cs
@@ -0,0 +1,32 @@
+﻿using BizHawk.Emulation.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizHawk.Emulation.Cores.Nintendo.Dolphin
+{
+	public partial class Dolphin : IDebuggable
+	{
+		[FeatureNotImplemented]
+		public IDictionary<string, RegisterValue> GetCpuFlagsAndRegisters()
+		{
+			throw new NotImplementedException();
+		}
+
+		[FeatureNotImplemented]
+		public void SetCpuRegister(string register, int value)
+		{
+			throw new NotImplementedException();
+		}
+
+		[FeatureNotImplemented]
+		public IMemoryCallbackSystem MemoryCallbacks { get; }
+
+		public bool CanStep(StepType type) { return false; }
+
+		[FeatureNotImplemented]
+		public void Step(StepType type) { throw new NotImplementedException(); }
+
+		public long TotalExecutedCycles => (long)_core.Dolphin_GetTicks();
+	}
+}
